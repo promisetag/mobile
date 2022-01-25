f053d3b857ab2af05910a73a280bc5d20eb25794
Amit Kumar Dubey <guttume@outlook.com>
2022-01-26 00:26:37 +0530

    added custom fonts

--- App.js
+++ App.js
@@ -5,8 +5,106 @@ import AppNavigator from "./navigation";
 import BottomDrawer from "./components/BottomDrawer";
 import { NativeBaseProvider } from "native-base";
 import borno from "./assets/theme";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts as useInterFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import {
+  useFonts as userRobotoFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
+import {
+  useFonts as useMontserratFonts,
+  Montserrat_100Thin,
+  Montserrat_100Thin_Italic,
+  Montserrat_200ExtraLight,
+  Montserrat_200ExtraLight_Italic,
+  Montserrat_300Light,
+  Montserrat_300Light_Italic,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_500Medium,
+  Montserrat_500Medium_Italic,
+  Montserrat_600SemiBold,
+  Montserrat_600SemiBold_Italic,
+  Montserrat_700Bold,
+  Montserrat_700Bold_Italic,
+  Montserrat_800ExtraBold,
+  Montserrat_800ExtraBold_Italic,
+  Montserrat_900Black,
+  Montserrat_900Black_Italic,
+} from "@expo-google-fonts/montserrat";
 
 export default function App() {
+  let [robotoFontsLoaded] = userRobotoFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    Roboto_900Black_Italic,
+  });
+  let [interFontsLoaded] = useInterFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+  let [montserratFontsLoaded] = useMontserratFonts({
+    Montserrat_100Thin,
+    Montserrat_100Thin_Italic,
+    Montserrat_200ExtraLight,
+    Montserrat_200ExtraLight_Italic,
+    Montserrat_300Light,
+    Montserrat_300Light_Italic,
+    Montserrat_400Regular,
+    Montserrat_400Regular_Italic,
+    Montserrat_500Medium,
+    Montserrat_500Medium_Italic,
+    Montserrat_600SemiBold,
+    Montserrat_600SemiBold_Italic,
+    Montserrat_700Bold,
+    Montserrat_700Bold_Italic,
+    Montserrat_800ExtraBold,
+    Montserrat_800ExtraBold_Italic,
+    Montserrat_900Black,
+    Montserrat_900Black_Italic,
+  });
+
+  if (!interFontsLoaded || !robotoFontsLoaded || !montserratFontsLoaded)
+    return <AppLoading />;
+
   return (
     <NativeBaseProvider theme={borno}>
       <ApplicationProvider>
--- assets/theme/fonts.js
+++ assets/theme/fonts.js
@@ -1,40 +1,118 @@
 const fonts = {
-  heading: "Roboto",
-  body: "Roboto",
-  mono: "Roboto",
+  heading: "Montserrat",
+  body: "Inter",
+  mono: "Montserrat",
 };
 
 const fontConfig = {
-  Roboto: {
+  Montserrat: {
     100: {
-      normal: "Roboto-Light",
-      italic: "Roboto-LightItalic",
+      normal: "Montserrat_100Thin",
+      italic: "Montserrat_100Thin_Italic",
     },
     200: {
-      normal: "Roboto-Light",
-      italic: "Roboto-LightItalic",
+      normal: "Montserrat_200ExtraLight",
+      italic: "Montserrat_200ExtraLight_Italic",
     },
     300: {
-      normal: "Roboto-Light",
-      italic: "Roboto-LightItalic",
+      normal: "Montserrat_300Light",
+      italic: "Montserrat_300Light_Italic",
     },
     400: {
-      normal: "Roboto",
-      italic: "Roboto-Italic",
+      normal: "Montserrat_400Regular",
+      italic: "Montserrat_400Regular_Italic",
     },
     500: {
-      normal: "Roboto-Medium",
+      normal: "Montserrat_500Medium",
+      italic: "Montserrat_500Medium_Italic",
     },
     600: {
-      normal: "Roboto-Medium",
-      italic: "Roboto-MediumItalic",
+      normal: "Montserrat_600SemiBold",
+      italic: "Montserrat_600SemiBold_Italic",
     },
     700: {
-      normal: "Roboto-Bold",
+      normal: "Montserrat_700Bold",
+      italic: "Montserrat_700Bold_Italic",
     },
     800: {
-      normal: "Roboto-Bold",
-      italic: "Roboto-BoldItalic",
+      normal: "Montserrat_800ExtraBold",
+      italic: "Montserrat_800ExtraBold_Italic",
+    },
+    900: {
+      normal: "Montserrat_900Black",
+      italic: "Montserrat_900Black_Italic",
+    },
+  },
+  Inter: {
+    100: {
+      normal: "Inter_100Thin",
+      italic: "Inter_100Thin",
+    },
+    200: {
+      normal: "Inter_200ExtraLight",
+      italic: "Inter_200ExtraLight",
+    },
+    300: {
+      normal: "Inter_300Light",
+      italic: "Inter_300Light",
+    },
+    400: {
+      normal: "Inter_400Regular",
+      italic: "Inter_400Regular",
+    },
+    500: {
+      normal: "Inter_500Medium",
+      italic: "Inter_500Medium",
+    },
+    600: {
+      normal: "Inter_600SemiBold",
+      italic: "Inter_600SemiBold",
+    },
+    700: {
+      normal: "Inter_700Bold",
+      italic: "Inter_700Bold",
+    },
+    800: {
+      normal: "Inter_800ExtraBold",
+      italic: "Inter_800ExtraBold",
+    },
+    900: {
+      normal: "Inter_900Black",
+      italic: "Inter_900Black",
+    },
+  },
+  Roboto: {
+    100: {
+      normal: "Roboto_100Thin",
+      italic: "Roboto_100Thin_Italic",
+    },
+    200: {
+      normal: "Roboto_100Thin",
+      italic: "Roboto_100Thin_Italic",
+    },
+    300: {
+      normal: "Roboto_300Light",
+      italic: "Roboto_300Light_Italic",
+    },
+    400: {
+      normal: "Roboto_400Regular",
+      italic: "Roboto_400Regulat_Italic",
+    },
+    500: {
+      normal: "Roboto_500Medium",
+      italic: "Roboto_500Medium_Italic",
+    },
+    600: {
+      normal: "Roboto_500Medium",
+      italic: "Roboto_500Medium_Italic",
+    },
+    700: {
+      normal: "Roboto_700Bold",
+      italic: "Roboto_700Bold_Italic",
+    },
+    800: {
+      normal: "Roboto-900Black",
+      italic: "Roboto_900Black_Italic",
     },
     900: {
       normal: "Roboto-Bold",
