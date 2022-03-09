85d9bb9a743d5cac569bea209778a224db977975
Amit Kumar Dubey <guttume@outlook.com>
2022-03-09 22:30:10 +0530

    allow users to pick media

--- components/UploadMemories.js
+++ components/UploadMemories.js
@@ -1,4 +1,5 @@
 import { Feather } from "@expo/vector-icons";
+import * as DocumentPicker from "expo-document-picker";
 import {
   Actionsheet,
   Box,
@@ -6,11 +7,19 @@ import {
   HStack,
   Icon,
   Image,
+  Pressable,
   Text,
   VStack,
 } from "native-base";
 
 const UploadMemories = ({ isOpen, setIsOpen }) => {
+  const launchPicker = async (type) => {
+    const files = await DocumentPicker.getDocumentAsync({
+      copyToCacheDirectory: false,
+      type: type,
+    });
+    console.log(files);
+  };
   return (
     <Actionsheet
       hideDragIndicator={true}
@@ -23,86 +32,94 @@ const UploadMemories = ({ isOpen, setIsOpen }) => {
             <Icon as={Feather} name="upload" />
             <Heading size="md">Upload media files</Heading>
           </HStack>
-          <HStack
-            width="full"
-            space="8"
-            borderColor="gray.300"
-            borderWidth="2"
-            p="2"
-            borderRadius="md"
-          >
-            <Box maxW="60%">
-              <Heading size="sm">Upload video</Heading>
-              <Text>This is a sample text used for this app</Text>
-            </Box>
-            <Box>
-              <Image
-                source={require("../assets/images/memories/Video.png")}
-                alt="image"
-                size="sm"
-              />
-            </Box>
-          </HStack>
-          <HStack
-            width="full"
-            space="8"
-            borderColor="gray.300"
-            borderWidth="2"
-            p="2"
-            borderRadius="md"
-          >
-            <Box maxW="60%">
-              <Heading size="sm">Upload pictures</Heading>
-              <Text>This is a sample text used for this app</Text>
-            </Box>
-            <Box>
-              <Image
-                source={require("../assets/images/memories/Image.png")}
-                alt="image"
-                size="sm"
-              />
-            </Box>
-          </HStack>
-          <HStack
-            width="full"
-            space="8"
-            borderColor="gray.300"
-            borderWidth="2"
-            p="2"
-            borderRadius="md"
-          >
-            <Box maxW="60%">
-              <Heading size="sm">Upload audio</Heading>
-              <Text>This is a sample text used for this app</Text>
-            </Box>
-            <Box>
-              <Image
-                source={require("../assets/images/memories/Audio.png")}
-                alt="image"
-                size="sm"
-              />
-            </Box>
-          </HStack>
-          <HStack
-            width="full"
-            space="8"
-            borderColor="gray.300"
-            borderWidth="2"
-            p="2"
-            borderRadius="md"
-          >
-            <Box maxW="60%">
-              <Heading size="sm">Upload text</Heading>
-              <Text>This is a sample text used for this app</Text>
-            </Box>
-            <Box>
-              <Image
-                source={require("../assets/images/memories/T.png")}
-                alt="image"
-                size="sm"
-              />
-            </Box>
-          </HStack>
+          <Pressable onPress={() => launchPicker("video/*")}>
+            <HStack
+              width="full"
+              space="8"
+              borderColor="gray.300"
+              borderWidth="2"
+              p="2"
+              borderRadius="md"
+            >
+              <Box maxW="60%">
+                <Heading size="sm">Upload video</Heading>
+                <Text>This is a sample text used for this app</Text>
+              </Box>
+              <Box>
+                <Image
+                  source={require("../assets/images/memories/Video.png")}
+                  alt="image"
+                  size="sm"
+                />
+              </Box>
+            </HStack>
+          </Pressable>
+          <Pressable onPress={() => launchPicker("image/*")}>
+            <HStack
+              width="full"
+              space="8"
+              borderColor="gray.300"
+              borderWidth="2"
+              p="2"
+              borderRadius="md"
+            >
+              <Box maxW="60%">
+                <Heading size="sm">Upload pictures</Heading>
+                <Text>This is a sample text used for this app</Text>
+              </Box>
+              <Box>
+                <Image
+                  source={require("../assets/images/memories/Image.png")}
+                  alt="image"
+                  size="sm"
+                />
+              </Box>
+            </HStack>
+          </Pressable>
+          <Pressable onPress={() => launchPicker("audio/*")}>
+            <HStack
+              width="full"
+              space="8"
+              borderColor="gray.300"
+              borderWidth="2"
+              p="2"
+              borderRadius="md"
+            >
+              <Box maxW="60%">
+                <Heading size="sm">Upload audio</Heading>
+                <Text>This is a sample text used for this app</Text>
+              </Box>
+              <Box>
+                <Image
+                  source={require("../assets/images/memories/Audio.png")}
+                  alt="image"
+                  size="sm"
+                />
+              </Box>
+            </HStack>
+          </Pressable>
+          <Pressable onPress={() => launchPicker("application/*")}>
+            <HStack
+              width="full"
+              space="8"
+              borderColor="gray.300"
+              borderWidth="2"
+              p="2"
+              borderRadius="md"
+            >
+              <Box maxW="60%">
+                <Heading size="sm">Upload text</Heading>
+                <Text>This is a sample text used for this app</Text>
+              </Box>
+              <Box>
+                <Image
+                  source={require("../assets/images/memories/T.png")}
+                  alt="image"
+                  size="sm"
+                />
+              </Box>
+            </HStack>
+          </Pressable>
         </VStack>
       </Actionsheet.Content>
     </Actionsheet>
