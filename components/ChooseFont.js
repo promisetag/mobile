06b7ff099f24df481536d384fabf8354ade7d6b4
Amit Kumar Dubey <guttume@outlook.com>
2022-03-10 14:10:47 +0530

    allow users to customize their name

--- components/ChooseFont.js
+++ components/ChooseFont.js
@@ -60,6 +60,8 @@ import { useState } from "react";
 
 const ChooseFont = ({
   font,
+  size,
+  setSize,
   setFont,
   alignment,
   setAlignment,
@@ -69,7 +71,7 @@ const ChooseFont = ({
   setIsOpen,
 }) => {
   const [family, setFamily] = useState("Roboto");
-  const [weight, setWeight] = useState("400Regular");
+  const [weight, setWeight] = useState("_400Regular");
   const [style, setStyle] = useState("");
 
   setFont(`${family}${weight}${style}`);
@@ -115,6 +117,29 @@ const ChooseFont = ({
     Roboto_900Black,
     Roboto_900Black_Italic,
   });
+
+  const FONT_SIZES = [
+    {
+      label: "Extra Small",
+      value: "xs",
+    },
+    {
+      label: "Small",
+      value: "sm",
+    },
+    {
+      label: "Medium",
+      value: "md",
+    },
+    {
+      label: "Large",
+      value: "lg",
+    },
+    {
+      label: "Extra Large",
+      value: "xl",
+    },
+  ];
 
   const FONT_LIST = [
     {
@@ -217,107 +242,154 @@ const ChooseFont = ({
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
       >
-        <Actionsheet.Content alignItems="flex-start">
-          <VStack space="8" my="4">
+        <Actionsheet.Content alignItems="flex-start" pl="4">
+          <VStack space="8" my="4" w="full">
             <HStack space="2">
-              <Heading size="md">Choose Font</Heading>
+              <Heading size="md">Customize your name</Heading>
             </HStack>
-            <VStack space="8" w="75%">
-              <Box>
-                <Heading size="sm">Font family</Heading>
-                <Select
-                  selectedValue={family}
-                  onValueChange={(itemValue) => setFamily(itemValue)}
-                >
-                  {FONT_LIST.map((font) => (
-                    <Select.Item
-                      fontFamily={`${font.family}_400Regular`}
-                      label={font.family}
-                      value={font.family}
-                      key={font.family}
-                    />
-                  ))}
-                </Select>
-              </Box>
-              <Box>
-                <Heading size="sm">Font weight</Heading>
-                <Select
-                  selectedValue="400Regular"
-                  onValueChange={(itemValue) => setWeight(itemValue)}
-                >
-                  {FONT_LIST.find((font) => font.family === family).weights.map(
-                    (weight) => (
+            <VStack space="8">
+              <HStack space="4">
+                <Box w="45%">
+                  <Heading size="sm" mb="2">
+                    Font family
+                  </Heading>
+                  <Select
+                    selectedValue={family}
+                    onValueChange={(itemValue) => setFamily(itemValue)}
+                  >
+                    {FONT_LIST.map((font) => (
+                      <Select.Item
+                        fontFamily={`${font.family}_400Regular`}
+                        label={font.family}
+                        value={font.family}
+                        key={font.family}
+                      />
+                    ))}
+                  </Select>
+                </Box>
+                <Box w="45%">
+                  <Heading size="sm" mb="2">
+                    Font size
+                  </Heading>
+                  <Select
+                    selectedValue={size}
+                    onValueChange={(itemValue) => setSize(itemValue)}
+                  >
+                    {FONT_SIZES.map((fsize) => (
+                      <Select.Item
+                        fontSize={size}
+                        label={fsize.label}
+                        value={fsize.value}
+                        key={fsize.value}
+                      />
+                    ))}
+                  </Select>
+                </Box>
+              </HStack>
+              <HStack space="4">
+                <Box w="45%">
+                  <Heading size="sm" mb="2">
+                    Font weight
+                  </Heading>
+                  <Select
+                    selectedValue={weight}
+                    onValueChange={(itemValue) => setWeight(itemValue)}
+                  >
+                    {FONT_LIST.find(
+                      (font) => font.family === family
+                    ).weights.map((weight) => (
                       <Select.Item
                         label={weight.name}
                         value={weight.value}
                         key={weight.name}
                       />
-                    )
-                  )}
-                </Select>
-              </Box>
-              <Box>
-                <Heading size="sm">Font style</Heading>
-                <Select
-                  selectedValue=""
-                  onValueChange={(itemValue) => setWeight(itemValue)}
-                >
-                  {FONT_LIST.find((font) => font.family === family).styles.map(
-                    (style) => (
+                    ))}
+                  </Select>
+                </Box>
+                <Box w="45%">
+                  <Heading size="sm" mb="2">
+                    Font style
+                  </Heading>
+                  <Select
+                    selectedValue={style}
+                    onValueChange={(itemValue) => setStyle(itemValue)}
+                  >
+                    {FONT_LIST.find(
+                      (font) => font.family === family
+                    ).styles.map((style) => (
                       <Select.Item
                         label={style.name}
                         value={style.value}
                         key={style.name}
                       />
-                    )
-                  )}
-                </Select>
-              </Box>
-              <Box>
-                <Heading size="sm">Vertical alignment</Heading>
-                <HStack>
-                  <IconButton
-                    onPress={() => setVerticalAlignment("top")}
-                    icon={<Icon as={MaterialIcons} name="align-vertical-top" />}
-                    size="sm"
-                  />
-                  <IconButton
-                    onPress={() => setVerticalAlignment("center")}
-                    icon={
-                      <Icon as={MaterialIcons} name="align-vertical-center" />
-                    }
-                    size="sm"
-                  />
-                  <IconButton
-                    onPress={() => setVerticalAlignment("bottom")}
-                    icon={
-                      <Icon as={MaterialIcons} name="align-vertical-bottom" />
-                    }
-                    size="sm"
-                  />
-                </HStack>
-              </Box>
-              <Box>
-                <Heading size="sm">Alignment</Heading>
-                <HStack>
-                  <IconButton
-                    onPress={() => setAlignment("left")}
-                    icon={<Icon as={Feather} name="align-left" />}
-                    size="sm"
-                  />
-                  <IconButton
-                    onPress={() => setAlignment("center")}
-                    icon={<Icon as={Feather} name="align-center" />}
-                    size="sm"
-                  />
-                  <IconButton
-                    onPress={() => setAlignment("right")}
-                    variant={alignment === "right" ? "outline" : "ghost"}
-                    icon={<Icon as={Feather} name="align-right" />}
-                    size="sm"
-                  />
-                </HStack>
-              </Box>
+                    ))}
+                  </Select>
+                </Box>
+              </HStack>
+              <HStack w="full" space="4">
+                <Box w="45%">
+                  <Heading size="sm" mb="4">
+                    Vertical alignment
+                  </Heading>
+                  <HStack space="4">
+                    <IconButton
+                      onPress={() => setVerticalAlignment("flex-start")}
+                      variant={
+                        verticalAlignment === "flex-start" ? "outline" : "ghost"
+                      }
+                      icon={
+                        <Icon as={MaterialIcons} name="align-vertical-top" />
+                      }
+                      size="sm"
+                    />
+                    <IconButton
+                      onPress={() => setVerticalAlignment("center")}
+                      variant={
+                        verticalAlignment === "center" ? "outline" : "ghost"
+                      }
+                      icon={
+                        <Icon as={MaterialIcons} name="align-vertical-center" />
+                      }
+                      size="sm"
+                    />
+                    <IconButton
+                      onPress={() => setVerticalAlignment("flex-end")}
+                      variant={
+                        verticalAlignment === "flex-end" ? "outline" : "ghost"
+                      }
+                      icon={
+                        <Icon as={MaterialIcons} name="align-vertical-bottom" />
+                      }
+                      size="sm"
+                    />
+                  </HStack>
+                </Box>
+                <Box>
+                  <Heading size="sm" mb="4">
+                    Alignment
+                  </Heading>
+                  <HStack space="4">
+                    <IconButton
+                      onPress={() => setAlignment("flex-start")}
+                      variant={alignment === "flex-start" ? "outline" : "ghost"}
+                      icon={<Icon as={Feather} name="align-left" />}
+                      size="sm"
+                    />
+                    <IconButton
+                      onPress={() => setAlignment("center")}
+                      variant={alignment === "center" ? "outline" : "ghost"}
+                      icon={<Icon as={Feather} name="align-center" />}
+                      size="sm"
+                    />
+                    <IconButton
+                      onPress={() => setAlignment("flex-end")}
+                      variant={alignment === "flex-end" ? "outline" : "ghost"}
+                      icon={<Icon as={Feather} name="align-right" />}
+                      size="sm"
+                    />
+                  </HStack>
+                </Box>
+              </HStack>
             </VStack>
           </VStack>
         </Actionsheet.Content>
